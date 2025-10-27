a7647d3d4e543fa1e1c685fc4b17f9da24989230
mategonzalez <mategonzalez@itba.edu.ar>
2025-10-27 11:10:52 -0300

    TP5: Consigna 11

---
+++ 11_InfoMed_TP4_BassaniGonzalezMenendez.sql
@@ -0,0 +1,9 @@
+SELECT 
+    p.nombre AS paciente,
+    c.fecha,
+    c.diagnostico
+FROM Consultas c
+JOIN Pacientes p ON p.id_paciente = c.id_paciente
+WHERE c.fecha >= '2024-08-01'
+  AND c.fecha < '2024-09-01'
+ORDER BY c.fecha;
