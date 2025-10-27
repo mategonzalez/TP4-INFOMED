ed4dd3c1721f0e09944b96c6a30a2696a736c454
mategonzalez <mategonzalez@itba.edu.ar>
2025-10-27 11:10:57 -0300

    TP5: Consigna 13

---
+++ 13_InfoMed_TP4_BassaniGonzalezMenendez.sql
@@ -0,0 +1,8 @@
+SELECT
+  p.nombre AS paciente,
+  COUNT(r.id_receta) AS cantidad_recetas
+FROM Pacientes p
+LEFT JOIN Recetas r
+  ON r.id_paciente = p.id_paciente
+GROUP BY p.nombre
+ORDER BY cantidad_recetas DESC, p.nombre;
